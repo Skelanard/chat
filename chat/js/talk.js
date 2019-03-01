3d2345571411372d09741cb8dde5f8929a16d480
Aleksandr Petukhov <37211674+Skelanard@users.noreply.github.com>
2019-03-01 12:34:07 +0100

    Update talk.js

--- chat/js/talk.js
+++ chat/js/talk.js
@@ -1,18 +1,19 @@
 function talk() {
-    var isFound = new Boolean(false); //deklaruję boolean'a który będzie sprawdzał czy element został znaleziony
-    var user = document.getElementById("userBox").value;
+    var isFound = new Boolean(false); //deklaruje boolean'a który będzie sprawdzał czy element został znaleziony
+    var user = document.getElementById("userBox").value;// otrzymanie wartości frazy użytkownika, wpisanej do okna dialogu
     if (user === "") {
         return;
-    }
+    }//blokada wysłanie wiadomości pustej
 
     document.getElementById("userBox").value = "";
-    document.getElementById("chatLog").innerHTML += ja + user + "<br>";
+    document.getElementById("chatLog").innerHTML += ja + user + "<br>";//funckcja wyświetlania wiadomości użytkownika w oknie chata
     Object.keys(hello).forEach(element => {
         if (user.toLowerCase().includes(element)) {
             console.log('znaleziono w tablicy hello')
             document.getElementById("chatLog").innerHTML += ryszard + hi + "<br>";
-            isFound = !isFound;
-            /*element został znaleziony, dla tego wartość isFound ma zmienić się na true.
+            /*przeszukuję każdyj element w objekcie, w razie obecności frazy w kluczach zwraca odpowiedź, zadeklarowaną w plikie,
+            zawierającym objekt*/
+            isFound = !isFound; /*element został znaleziony, dla tego wartość isFound ma zmienić się na true.
             Objekty poniżej powinni działać na takiej samej zasadzie, jednak wygląda na to, iż wartość się nie zmienia */
         }
     });
@@ -20,14 +21,20 @@ function talk() {
         if (user.toLowerCase().includes(element)) {
             console.log('znaleziono w tablicy who')
             document.getElementById("chatLog").innerHTML += ryszard + iam + "<br>";
-            isFound = !isFound;
+         /*przeszukuję każdyj element w objekcie, w razie obecności frazy w kluczach zwraca odpowiedź, zadeklarowaną w plikie,
+            zawierającym objekt*/
+            isFound = !isFound; /*element został znaleziony, dla tego wartość isFound ma zmienić się na true.
+            Objekty poniżej powinni działać na takiej samej zasadzie, jednak wygląda na to, iż wartość się nie zmienia */
         }
     });
     Object.keys(howareyou).forEach(element => {
         if (user.toLowerCase().includes(element)) {
             console.log('znaleziono w tablicy howareyou')
             document.getElementById("chatLog").innerHTML += ryszard + iamok + "<br>";
-            isFound = !isFound;
+           /*przeszukuję każdyj element w objekcie, w razie obecności frazy w kluczach zwraca odpowiedź, zadeklarowaną w plikie,
+            zawierającym objekt*/
+            isFound = !isFound; /*element został znaleziony, dla tego wartość isFound ma zmienić się na true.
+            Objekty poniżej powinni działać na takiej samej zasadzie, jednak wygląda na to, iż wartość się nie zmienia */
         }
 
     });
@@ -35,21 +42,40 @@ function talk() {
         if (user.toLowerCase().includes(element)) {
             console.log('znaleziono w tablicy ok')
             document.getElementById("chatLog").innerHTML += ryszard + ofcourse + "<br>";
-            isFound = !isFound;
+            /*przeszukuję każdyj element w objekcie, w razie obecności frazy w kluczach zwraca odpowiedź, zadeklarowaną w plikie,
+            zawierającym objekt*/
+            isFound = !isFound; /*element został znaleziony, dla tego wartość isFound ma zmienić się na true.
+            Objekty poniżej powinni działać na takiej samej zasadzie, jednak wygląda na to, iż wartość się nie zmienia */
         }
     });
-    Object.keys(schedule).forEach(element => {
+    Object.keys(time).forEach(element => {
+        if (user.toLowerCase().includes(element)) {
+            console.log('znaleziono w tablicy timee')
+            document.getElementById("chatLog").innerHTML += ryszard + timenow + "<br>";
+            /*przeszukuję każdyj element w objekcie, w razie obecności frazy w kluczach zwraca odpowiedź, zadeklarowaną w plikie,
+            zawierającym objekt*/
+            isFound = !isFound; /*element został znaleziony, dla tego wartość isFound ma zmienić się na true.
+            Objekty poniżej powinni działać na takiej samej zasadzie, jednak wygląda na to, iż wartość się nie zmienia */
+        }
+    });
+        Object.keys(schedule).forEach(element => {
         if (user.toLowerCase().includes(element)) {
             console.log('znaleziono w tablicy schedule')
             document.getElementById("chatLog").innerHTML += ryszard + schedulecheck + "<br>";
-            isFound = !isFound;
+            /*przeszukuję każdyj element w objekcie, w razie obecności frazy w kluczach zwraca odpowiedź, zadeklarowaną w plikie,
+            zawierającym objekt*/
+            isFound = !isFound; /*element został znaleziony, dla tego wartość isFound ma zmienić się na true.
+            Objekty poniżej powinni działać na takiej samej zasadzie, jednak wygląda na to, iż wartość się nie zmienia */
         }
     });
     Object.keys(parking).forEach(element => {
         if (user.toLowerCase().includes(element)) {
             console.log('znaleziono w tablicy parking')
             document.getElementById("chatLog").innerHTML += ryszard + parkingok + "<br>";
-            isFound = !isFound;
+            /*przeszukuję każdyj element w objekcie, w razie obecności frazy w kluczach zwraca odpowiedź, zadeklarowaną w plikie,
+            zawierającym objekt*/
+            isFound = !isFound; /*element został znaleziony, dla tego wartość isFound ma zmienić się na true.
+            Objekty poniżej powinni działać na takiej samej zasadzie, jednak wygląda na to, iż wartość się nie zmienia */
 
         }
     });
@@ -62,6 +88,6 @@ function talk() {
                 a gdy się nie znalazł to nic nie działa*/
 
     }
-    window.scrollTo(0, document.body.offsetHeight)
+    window.scrollTo(0, document.body.offsetHeight)//przejście biegunka na dół strony po każdym wysłaniu wiadomości
 
 }
