67a932675577b6d1eb7090f81e9c71fcdaaeadac
Aleksandr Petukhov <37211674+Skelanard@users.noreply.github.com>
2019-03-01 11:34:54 +0100

    Update parking.js

--- chat/js/parking.js
+++ chat/js/parking.js
@@ -1,7 +1,7 @@
 var parkingok = "W sumie, jeśli jest mijesce i jesteś studentem, to możesz zaparkować samochód na dziedziniću.<br>" +
-    "Ale pamiętaj, że wykładowcy i pracownicy uczelni mają pierwszeńśtwo🚗";
-var parking = {
-
+    "Ale pamiętaj, że wykładowcy i pracownicy uczelni mają pierwszeńśtwo🚗";/*odpowiedź chatbota na zapytanie o możliwości parkingu 
+    na terenie uczelni*/
+var parking = {//zbiór fraz zapytań o możliwości parkingu na terenie uczelni
     "parking": "",
     "parkingiem": "",
     "samochód?": "",
