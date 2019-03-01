d54e5e3f0b650ca2396d5dfebcd33ea18aca73c4
Aleksandr Petukhov <37211674+Skelanard@users.noreply.github.com>
2019-03-01 11:15:10 +0100

    Update exams.js

--- chat/js/exams.js
+++ chat/js/exams.js
@@ -1,10 +1,13 @@
 var deanery = " Na takie pytanie odpowie Ci dziekanat - albo <a href='https://dziekanat.wsei.edu.pl/Index/Index' target='_blank'>tutaj, wirtualny</a>, " +
     "albo Panie z dziekanatu realnego. <br>Żeby tam trafić, to wychodzisz na dziedziniec, trymasz się lewej storny i zobaczysz wydział administracyjny. Wchodzisz tam, idziesz prosto i zauważysz drzwi. "
-var exams = {
+//odpowiedź na zapytanie o terminach sesji z podaniem linka do wirtualnego dziekanatu
+var exams = {//zbiór fraz zapytań o terminach sesji
     "Sesją": "",
     "Kiedy mamy sesję?": "",
     "Egzaminy": "",
     "Kiedy mamy egzaminy?": "",
     "Kiedy mam egzaminy?": "",
+    "Terminy egzaminów": "",
+    "Terminy sesji: "",
 
 };
