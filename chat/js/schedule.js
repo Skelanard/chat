ba63a31434e30b31a20a24e4a0c75946fbbcbd9b
Aleksandr Petukhov <37211674+Skelanard@users.noreply.github.com>
2019-03-01 11:36:59 +0100

    Update schedule.js

--- chat/js/schedule.js
+++ chat/js/schedule.js
@@ -1,20 +1,13 @@
 var schedulecheck = "To zawszę może sprawdzić sobie <a href='https://harmonogramy.wsei.edu.pl' target='_blank'>tutaj, pod tym linkiem</a>." +
-    "Najlepiej sprawdzaj to codziennie, żeby być w kursie wszystkich zmian."
+    "Najlepiej sprawdzaj to codziennie, żeby być w kursie wszystkich zmian."//odpowiedź na zapytanie o zajęćiach, link na harmonogram
 
-var schedule = {
-    "Zajęcia": "",
+var schedule = {//zbiór zapytań o zajęciach
     "zajęcia": "",
-    "Zajecia": "",
     "zajecia": "",
-    "Jakie dzisiaj mam zajęcia?": "",
-    "Jakie dziś mam zajęcia?": "",
-    "Jakie dzis mam zajecia?": "",
-    "Co tam dzisiaj?": "",
+    "co tam dziśiaj?": "",
     "jakie dzisiaj mam zajęcia?": "",
     "jakie dziś mam zajęcia?": "",
     "jakie dzis mam zajecia?": "",
     "co tam dzisiaj?": "",
-    "Harmonogram": "",
-    "Harmonogram zajęć": "",
-
+    "harmonogram": "",
 };
