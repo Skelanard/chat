a00496ee2e681d2da05315e6d21ca30617b1890f
Aleksandr Petukhov <37211674+Skelanard@users.noreply.github.com>
2019-03-01 11:18:58 +0100

    Update howareyou.js

--- chat/js/howareyou.js
+++ chat/js/howareyou.js
@@ -1,11 +1,11 @@
-var iamok = "A, wszystko w porządku. Wszystko gra, kierowniku.";
+var iamok = "A, wszystko w porządku. Wszystko gra, kierowniku."; //odpwoiedź na pytania rodzaju "co słychać"
 
-var howareyou = {
+var howareyou = {// zbiór fraz zapytań rodzaju "co słychać", niestety nie wiem jak to nazwać inczej :(
     "jak tam": "",
     "co tam": "",
     "co słychać": "",
     "co u Ciebie": "",
-    "jak Pan się czuję": "",
+    "jak pan się czuję": "",
     "co u Pana": "",
 
 };
