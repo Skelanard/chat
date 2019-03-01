687843f1db37f161911e63d096eaab2c2c1818dd
Aleksandr Petukhov <37211674+Skelanard@users.noreply.github.com>
2019-03-01 11:05:31 +0100

    Update ask.js

--- chat/js/ask.js
+++ chat/js/ask.js
@@ -1,5 +1,5 @@
-var hear = "Tak, słucham, mistrzu."
-var ask = {
+var hear = "Tak, słucham, mistrzu."//odpowiedź na prośbę o możliwości zapytania
+var ask = {//zbór fraz użytkownika z prośbą o możliwości zapytania
     "Mam pytanie": "",
     "Chciałbym Pana zapytać": "",
     "Chciałabym Pana zapytać": "",
