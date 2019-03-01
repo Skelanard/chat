119723fa3caf037e25341ba9b90b6dd86475ca75
Aleksandr Petukhov <37211674+Skelanard@users.noreply.github.com>
2019-03-01 12:23:28 +0100

    Update who.js

--- chat/js/who.js
+++ chat/js/who.js
@@ -1,13 +1,10 @@
 var iam = "Jestem portierem na Wyższej Szkole Ekonomii i Informatyki w Krakowie. Ale, jeśli tu studujesz to najprawdopodobniej o tym wiesz 😉";
-
-var who = {
-    "Kim Pan jest?": "",
+//odpowiedź na zapytanie kim Pan jest
+var who = {//zbiór pytań o tym kim Pan jest
     "Kim jesteś?": "",
-    "Kim Pan jest": "",
     "Kim jestes?": "",
     "Kim pan jest?": "",
     "kim jesteś?": "",
-    "kim pan jest": "",
     "Kim jestes?": "",
 
 };
