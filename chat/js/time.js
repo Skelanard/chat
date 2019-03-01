3cd3fd8a28bb4e99a0f3f76cc10cadc4b257d84b
Aleksandr Petukhov <37211674+Skelanard@users.noreply.github.com>
2019-03-01 12:19:31 +0100

    Update time.js

--- chat/js/time.js
+++ chat/js/time.js
@@ -1,14 +1,15 @@
-var t = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-var d = new Date().toLocaleDateString("pl", options);
+var t = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");//deklarowanie zmiennej czasu, formatowanie czasu      
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };//ustawienia formatowania dzisiejszej daty
+var d = new Date().toLocaleDateString("pl", options);//deklaracja dzisiejszej daty, ustawenie używanie polskiego formatu daty
 
 timenow = "Czekaj, już sprawdzam...To tak: " + t + ". No i dzisiaj jest " + d + " roku. Ale szybko ten czas leci...";
 
 
 
-var time = {
-    "Godzina": "",
-    "Czas": "",
-    "Która teraz jest godzina?": "",
-    "Który jest dzisiaj?": "",
+var time = {//zbiór fraz zapytań o aktualnym czasie i dzisiejszą datę
+    "godzina": "",
+    "czas": "",
+    "data": "",
+    "który jest dzisiaj?": "",
+    "która jest teraz?": "",
 }
