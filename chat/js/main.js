15925d64e418617b34b0bcd59e16865b61588eb0
Aleksandr Petukhov <37211674+Skelanard@users.noreply.github.com>
2019-03-01 11:26:57 +0100

    Update main.js

--- chat/js/main.js
+++ chat/js/main.js
@@ -4,10 +4,11 @@
     document.getElementsByTagName('head')[0].appendChild(script);
 };
 
-include("/chat/js/list.js");
+include("/chat/js/list.js");//impoirt plików .js z listy plików list.js
 
 
-var ja = "Ja: ";
-var ryszard = "Ryszard: ";
+var ja = "Ja: "; //tekst przed każdą wiadomościa użytkownika
+var ryszard = "Ryszard: ";//tekst przed każdą wiadomością chatbota
 var dontKnow = "Słuchaj, no na to nie jestem w stanie nic Ci tu powiedzieć. " +
     "Zadzwoń może na portiernię, numer <a href='tel:48124311890'>+48 12 431 18 90</a> a otrzymasz fachową i konkretną odpowiedź.";
+//odpowiedź w przypadku braku wariantu odpowiedźi chatbota na zapytanie użytkownika
